4e19560d81ff8925c2e04745ec05cbe020665b82
anicolao <anicolao@users.noreply.github.com>
2026-09-11 19:01:15 +0000

    deploy: 705c861969cd089d02de7517f1a1476e816a6fb8

---
+++ pr3/_app/immutable/nodes/0.BMwjJ-g9.js
@@ -0,0 +1,1 @@
+import{B as e,D as t,c as n,d as r,f as i}from"../chunks/BrmQJ5S9.js";import"../chunks/xihTtKlq.js";var a=e({prerender:()=>!0,trailingSlash:()=>o}),o=`always`;function s(e,a){var o=i(),s=t(o);n(s,()=>a.children),r(e,o)}export{s as component,a as universal};
